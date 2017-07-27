4421c1e503e880ace3130b3aef3683594abc80e9
Artyom Pervukhin <artyom@evasive.ru>
2017-07-27 13:45:57 +0300

    Vendor dependencies

---
+++ vendor/github.com/davecheney/loadavg/loadavg_stub.go
@@ -0,0 +1,12 @@
+// +build !linux,!darwin,!freebsd
+
+package loadavg
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func loadAvg() ([3]float64, error) {
+	return [...]float64{-1, -1, -1}, fmt.Errorf("loadavg: unsupported platform %q", runtime.GOOS)
+}
